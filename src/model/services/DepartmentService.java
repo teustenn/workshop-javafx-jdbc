47be5af5c27123b29ac5f54a6a4b066f90e9dd8a
teustenn <teustenn@gmail.com>
2022-10-30 10:24:24 -0300

    DepartmentService

---
+++ src/model/services/DepartmentService.java
@@ -0,0 +1,20 @@
+package model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentService {
+	
+	public List<Department> findAll() {
+		List<Department> list = new ArrayList<>();
+		
+		list.add(new Department(1, "Monitors"));
+		list.add(new Department(1, "Computers"));
+		list.add(new Department(1, "Videogames"));
+		
+		return list;
+	}
+
+}
